7905e962bae3e05c1393b4a6d02341cb4d796548
bGillooley <gillooley@gmail.com>
2023-02-13 18:06:16 +0000

    added train times component

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,17 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getTrains } from "@/lib/getTrains";
+import { XMLParser } from "fast-xml-parser";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const parser = new XMLParser();
   try {
-    let mitch = await fetch(
-      "https://api.kucoin.com/api/v1/market/stats?symbol=BTC-USDT"
+    const response = await fetch(
+      "https://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc=Skerries"
     );
-    const data = await mitch.json();
-    res.status(200).json(data);
+    const data = await response.text();
+    let jObj = await parser.parse(data);
+    let trains = jObj.ArrayOfObjStationData.objStationData;
+    let result = trains.map((result) => {
+      return {
+        direction: result.Direction,
+        arrival: result.Exparrival,
+        destination: result.Destination,
+      };
+    });
+    res.status(200).json(result);
   } catch (err) {
     console.log(err);
   }
