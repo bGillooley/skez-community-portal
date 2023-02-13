6677f9acb5893272049b3b24e0b9a61ef789dc1e
bGillooley <gillooley@gmail.com>
2023-02-13 15:31:17 +0000

    added train times component

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getTrains } from "@/lib/getTrains";
 
-type Data = {
-  name: string;
-};
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: "John Doe" });
+  let mitch = await getTrains();
+  res.status(200).json(mitch);
 }
