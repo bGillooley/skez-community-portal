c489d4690768f5f64f67cb78b79e160b95ed0394
bGillooley <gillooley@gmail.com>
2023-02-15 12:08:28 +0000

    testing web scraping

--- pages/api/billy.ts
+++ pages/api/billy.ts
@@ -1,16 +1,42 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
 import * as cheerio from "cheerio";
 
 interface TrainTimes {
   destination: string;
   departs: string;
 }
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
   let trains: TrainTimes[] = [];
   try {
     const response = await fetch(
