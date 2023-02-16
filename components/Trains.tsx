2855581e06fb5300ce12af3bdef798d66db0d3b1
bGillooley <gillooley@gmail.com>
2023-02-16 17:34:35 +0000

    train times

--- components/Trains.tsx
+++ components/Trains.tsx
@@ -3,8 +3,10 @@ import React, { useEffect, useState } from "react";
 const Trains = () => {
   const [northBoundTrains, setNorthBoundTrains] = useState([]);
   const [southBoundTrains, setSouthBoundTrains] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   function getTrains() {
+    setLoading(true);
     fetch("/api/trains", { cache: "no-cache" })
       .then((res) => res.json())
       .then((data) => {
@@ -12,6 +14,7 @@ const Trains = () => {
         setNorthBoundTrains(data.details1);
         setSouthBoundTrains(data.details2);
       });
+    setLoading(false);
     console.log("It ran..");
   }
 
@@ -28,7 +31,7 @@ const Trains = () => {
           return (
             <div key={e.eta}>
               <div>
-                Destination: {e.destination} | Due in: {e.duein}
+                Destination: {e.destination} | Departs: {e.eta}
               </div>
             </div>
           );
@@ -40,13 +43,28 @@ const Trains = () => {
           return (
             <div key={e.eta}>
               <div>
-                Destination: {e.destination} | Due in: {e.duein}
+                Destination: {e.destination} | Departs: {e.eta}
               </div>
             </div>
           );
         })}
       </div>
-      <button onClick={getTrains}>Get Next Trains</button>
+      {loading && <div>Loading there...</div>}
+      <div className="py-4">
+        <button
+          className="inline-flex justify-center rounded-lg text-sm font-semibold py-2.5 px-4 bg-slate-900 text-white hover:bg-slate-700"
+          onClick={getTrains}
+        >
+          Update Info
+        </button>
+        <a
+          className="inline-flex justify-center rounded-lg text-sm font-semibold py-2.5 px-4 bg-slate-900 text-white hover:bg-slate-700  ml-4"
+          href="https://www.irishrail.ie/en-ie/train-timetables/live-departure-train-times?key=skerries&REQ0JourneyStopskeyID=&HWAI%3DJS%21js=yes&HWAI%3DJS%21ajax=yes#live-departure-anchor"
+          target="_blank"
+        >
+          View on Irish Rail
+        </a>
+      </div>
     </>
   );
 };
